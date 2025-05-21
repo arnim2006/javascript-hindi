9b274c721d110eb73c3d2c32f676a8254f909350
arnim2006 <24ucc030@lnmiit.ac.in>
2025-05-21 10:24:17 +0000

    hey how are you doing

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+console.log("2" > 1);
+console.log( "02" > 1);
+
+console.log(null > 0);   // output = false
+console.log(null == 0);  // output = false
+console.log(null >= 0);  // output = true
+
+
+console.log(undefined == 0);  // output = true
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
